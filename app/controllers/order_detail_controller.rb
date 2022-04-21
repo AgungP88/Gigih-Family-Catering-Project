edfb7cb9b7c10a6e1db4d78f70f21ce5400faed8
Agung Prabowo <49488327+AgungP88@users.noreply.github.com>
2022-04-21 09:08:32 +0000

    add CRUD to orderDetails

--- app/controllers/order_detail_controller.rb
+++ app/controllers/order_detail_controller.rb
@@ -8,11 +8,35 @@ class OrderDetailController < ApplicationController
     @order_detail = OrderDetail.where(id: params[:id])
   end
 
+  def create
+    order_detail = OrderDetail.create(params.require(:order_detail).permit(:order_id, :menu_id, :quantity, :price))
+
+    redirect_to order_detail_index_path
+  end
+
   def new
     @order_detail = OrderDetail.new
   end
 
   def edit
     @order_detail = OrderDetail.where(id: params[:id]).first
+  end
+
+  def update
+    order_detail = OrderDetail.where(id: params[:id]).first
+    order_detail.update(params.require(:order_detail).permit(:order_id, :menu_id, :quantity, :price))
+    redirect_to order_detail_index_path
+  end
+
+  def destroy
+    @order_detail = OrderDetail.where(id: params[:id]).first
+    @order_detail.destroy
+
+    redirect_to order_detail_index_path
+  end
+
+  private
+  def order_detail_params
+    params.require(:order_detail).permit(:order_id, :menu_id, :quantity, :price)
   end
 end
