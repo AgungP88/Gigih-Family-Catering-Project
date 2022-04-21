e8c0ae59c43e9408f5258b05160a3a4dc1dea5f1
Agung Prabowo <49488327+AgungP88@users.noreply.github.com>
2022-04-21 09:19:45 +0000

    Add customary spec for Category model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with a name' do
+    category = Category.new(
+      name: 'Makanan'
+    )
+
+    expect(category).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    category = Category.create(
+      name: nil
+    )
+
+    category.valid?
+
+    expect(category.errors[:name]).to include("can't be blank")
+  end
+
+  it 'is invalid when name have less than or equal to 4 characters' do
+    category = Category.create(
+      name: 'tes'
+    )
+
+    category.valid?
+
+    expect(category.errors[:name]).to include("is too short (minimum is 4 characters)")
+  end
 end
