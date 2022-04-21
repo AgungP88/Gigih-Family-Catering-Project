cb0a15b9ca9a422a6134a5219afe03cd7399fadc
Agung Prabowo <49488327+AgungP88@users.noreply.github.com>
2022-04-21 09:41:19 +0000

    Add customary spec for Menu model

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -3,4 +3,12 @@ class Menu < ApplicationRecord
 
   has_many :order_details
   has_many :orders, through: :order_details
+
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :price, numericality: true, comparison: {greater_than_or_equal_to: 0.01}
+
+  def self.by_letter(letter)
+    where("name LIKE ?", "#{letter}%").order(:name)
+  end
 end
--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -1,5 +1,92 @@
 require 'rails_helper'
 
 RSpec.describe Menu, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with a name and a description' do
+    menu = Menu.new(
+      name: 'Nasi Uduk',
+      description: 'Betawi style steamed rice cooked in coconut milk. Delicious!',
+      price: 15000.0,
+      category_id: "1",
+    )
+
+    expect(menu).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    menu = Menu.new(
+      name: nil,
+      description: 'Betawi style steamed rice cooked in coconut milk. Delicious!',
+      price: 15000.0
+    )
+
+    menu.valid?
+
+    expect(menu.errors[:name]).to include("can't be blank")
+  end
+
+  it "is invalid with a duplicate name" do
+    menu1 = Menu.create(
+      name: "Nasi Uduk",
+      description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
+      price: 10000.0
+    )
+    
+    menu2 = Menu.new(
+      name: "Nasi Uduk",
+      description: "Just with a different description.",
+      price: 10000.0
+    )
+
+    menu2.valid?
+    
+    expect(menu2.errors[:name]).to include("has already been taken")
+  end
+  
+  it "does not accept non numeric value for price" do
+    menu = Menu.create(
+      name: "Nasi Uduk",
+      description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
+      price: "Prank!"
+    )
+
+    menu.valid?
+    
+    expect(menu.errors[:price]).to include("is not a number")
+  end
+
+  it "does not accept price less than 0.01" do
+    menu = Menu.create(
+      name: "Nasi Uduk",
+      description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
+      price: 0
+    )
+
+    menu.valid?
+    
+    expect(menu.errors[:price]).to include("must be greater than or equal to 0.01")
+  end
+
+  describe 'self#by_letter' do
+    it "should return a sorted array of results that match" do
+      menu1 = Menu.create(
+        name: "Nasi Uduk",
+        description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
+        price: 10000.0
+      )
+
+      menu2 = Menu.create(
+        name: "Kerak Telor",
+        description: "Betawi traditional spicy omelette made from glutinous rice cooked with egg and served with serundeng.",
+        price: 8000.0
+      )
+
+      menu3 = Menu.create(
+        name: "Nasi Semur Jengkol",
+        description: "Based on dongfruit, this menu promises a unique and delicious taste with a small hint of bitterness.",
+        price: 8000.0
+      )
+
+      expect(Menu.by_letter("N")).to eq([menu3, menu1])
+    end
+  end
 end
